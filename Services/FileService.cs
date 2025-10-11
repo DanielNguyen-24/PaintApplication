40085cd42246d3b1d119cf465303499e2d664be8
Nguyễn Thành Trung <140838446+ThanhTrunggDEV@users.noreply.github.com>
2025-10-11 12:52:10 +0700

    Fix toolbox bindings and service namespaces

--- Services/FileService.cs
+++ Services/FileService.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Microsoft.Win32;
 using PaintApplication.Models;
-using System.IO;
 
 namespace PaintApplication.Services
 {
-    class FileService
+    public class FileService
     {
         public ProjectModel? OpenProject()
         {
             var dlg = new OpenFileDialog { Filter = "Paint Project (*.json)|*.json" };
             if (dlg.ShowDialog() == true)
             {
-                var json = System.IO.File.ReadAllText(dlg.FileName);
-                return System.Text.Json.JsonSerializer.Deserialize<ProjectModel>(json);
+                var json = File.ReadAllText(dlg.FileName);
+                return JsonSerializer.Deserialize<ProjectModel>(json);
             }
             return null;
         }
--- ViewModels/ToolBoxViewModel.cs
+++ ViewModels/ToolBoxViewModel.cs
@@ -1,47 +1,44 @@
 ﻿using PaintApplication.Helpers;
+using PaintApplication.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 using System.Windows.Media;
-using PaintApplication.Models; 
-using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
-using System.ComponentModel;
-
 
 namespace PaintApplication.ViewModels
 {
     public class ToolboxViewModel : ViewModelBase
     {
-        // === State ===
-
         private ToolType _selectedTool = ToolType.Pencil;
         public ToolType SelectedTool
         {
             get => _selectedTool;
             set => SetProperty(ref _selectedTool, value);
         }
+
         private ShapeType _selectedShape = ShapeType.None;
         public ShapeType SelectedShape
         {
             get => _selectedShape;
             set
             {
-                if (SetProperty(ref _selectedShape, value))
+                if (SetProperty(ref _selectedShape, value) && value != ShapeType.None)
                 {
-                    if (value != ShapeType.None)
-                        SelectedTool = ToolType.Shape;
+                    SelectedTool = ToolType.Shape;
                 }
             }
         }
+
         private BrushType _selectedBrush = BrushType.Brush;
         public BrushType SelectedBrush
         {
             get => _selectedBrush;
             set
             {
-                if (SetProperty(ref _selectedBrush, value))
+                if (SetProperty(ref _selectedBrush, value) && value != BrushType.Brush)
                 {
-                    if (value != BrushType.Brush)
-                        SelectedTool = ToolType.Brush;
+                    SelectedTool = ToolType.Brush;
                 }
             }
         }
@@ -59,65 +56,68 @@ namespace PaintApplication.ViewModels
             get => _selectedColor;
             set => SetProperty(ref _selectedColor, value);
         }
-        private string _selectedFontFamily = "Arial";
-        private double _fontSize = 16;
 
+        private string _selectedFontFamily = "Arial";
         public string SelectedFontFamily
         {
             get => _selectedFontFamily;
-            set { _selectedFontFamily = value; OnPropertyChanged(); }
+            set => SetProperty(ref _selectedFontFamily, value);
         }
 
+        private double _fontSize = 16;
         public double FontSize
         {
             get => _fontSize;
-            set { _fontSize = value; OnPropertyChanged(); }
+            set => SetProperty(ref _fontSize, value);
         }
 
-        public List<string> FontFamilies { get; } =
-            Fonts.SystemFontFamilies.Select(f => f.Source).ToList();
+        public List<string> FontFamilies { get; } = Fonts.SystemFontFamilies.Select(f => f.Source).ToList();
 
-        public List<double> FontSizes { get; } = new List<double>
-    { 8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 48, 72 };
+        public List<double> FontSizes { get; } = new()
+        {
+            8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 48, 72
+        };
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        public ObservableCollection<BrushItem> Brushes { get; }
 
+        public ObservableCollection<Color> AvailableColors { get; } = new()
+        {
+            Colors.Black,
+            Colors.White,
+            Colors.Red,
+            Colors.Green,
+            Colors.Blue,
+            Colors.Yellow,
+            Colors.Orange,
+            Colors.Purple,
+            Colors.Brown,
+            Colors.Gray,
+            Colors.Pink,
+            Colors.Cyan,
+            Colors.Magenta,
+            Colors.Lime,
+            Colors.Gold,
+            Colors.Silver,
+            Colors.Navy,
+            Colors.Teal,
+            Colors.Maroon,
+            Colors.Olive
+        };
 
-        public ObservableCollection<BrushItem> Brushes { get; set; }
-
-
-        // === Commands ===
-
-        // Selection
         public ICommand SelectCommand { get; }
-
-        // Image
         public ICommand CropCommand { get; }
         public ICommand RotateCommand { get; }
-
-        // Tools
         public ICommand PencilCommand { get; }
         public ICommand EraserCommand { get; }
         public ICommand FillCommand { get; }
         public ICommand TextCommand { get; }
         public ICommand MagnifierCommand { get; }
-
-        // Brushes
         public ICommand BrushCommand { get; }
-
-        // Shapes
         public ICommand DrawCircleCommand { get; }
-
-        // Colors
         public ICommand OpenColorPickerCommand { get; }
-
-        // Copilot
         public ICommand AiCommand { get; }
-
-        // Layers
         public ICommand LayersCommand { get; }
+        public ICommand SelectBrushCommand { get; }
         public ICommand SelectColorCommand { get; }
 
         public ToolboxViewModel()
@@ -140,28 +140,40 @@ namespace PaintApplication.ViewModels
             OpenColorPickerCommand = new RelayCommand(_ => DoOpenColorPicker());
             AiCommand = new RelayCommand(_ => DoAI());
             LayersCommand = new RelayCommand(_ => DoLayers());
-            SelectCommand = new RelayCommand(_ => SelectedTool = ToolType.Select);
-            BrushCommand = new RelayCommand(_ =>
+
+            SelectBrushCommand = new RelayCommand(obj =>
             {
-                if (_ is BrushItem brush)
-                    _selectedBrush = brush.Type;
-                SelectedTool = ToolType.Brush;
+                if (obj is BrushItem brush)
+                {
+                    SelectedBrush = brush.Type;
+                    SelectedTool = ToolType.Brush;
+                }
             });
+
+            SelectColorCommand = new RelayCommand(obj =>
+            {
+                if (obj is Color color)
+                {
+                    SelectedColor = color;
+                }
+            });
+
             Brushes = new ObservableCollection<BrushItem>
-{
-    new BrushItem { Name = "Brush", Type = BrushType.Brush, IconPath = "pack://application:,,,/Resources/Icons/brush.png" },
-    //new BrushItem { Name = "Calligraphy brush", Type = BrushType.CalligraphyBrush, IconPath = "pack://application:,,,/Resources/Icons/calligraphy_brush.png" },
-    new BrushItem { Name = "Calligraphy pen", Type = BrushType.CalligraphyPen, IconPath = "pack://application:,,,/Resources/Icons/calligraphy_pen.png" },
-    new BrushItem { Name = "Airbrush", Type = BrushType.Airbrush, IconPath = "pack://application:,,,/Resources/Icons/airbrush.png" },
-    new BrushItem { Name = "Oil brush", Type = BrushType.OilBrush, IconPath = "pack://application:,,,/Resources/Icons/oil_brush.png" },
-    new BrushItem { Name = "Crayon", Type = BrushType.Crayon, IconPath = "pack://application:,,,/Resources/Icons/crayon.png" },
-    new BrushItem { Name = "Marker", Type = BrushType.Marker, IconPath = "pack://application:,,,/Resources/Icons/marker.png" },
-    new BrushItem { Name = "Natural pencil", Type = BrushType.NaturalPencil, IconPath = "pack://application:,,,/Resources/Icons/natural_pencil.png" },
-    new BrushItem { Name = "Watercolor brush", Type = BrushType.WatercolorBrush, IconPath = "pack://application:,,,/Resources/Icons/watercolor.png" }
-};
+            {
+                new BrushItem { Name = "Brush", Type = BrushType.Brush, IconPath = "pack://application:,,,/Resources/Icons/brush.png" },
+                new BrushItem { Name = "Calligraphy pen", Type = BrushType.CalligraphyPen, IconPath = "pack://application:,,,/Resources/Icons/calligraphy_pen.png" },
+                new BrushItem { Name = "Airbrush", Type = BrushType.Airbrush, IconPath = "pack://application:,,,/Resources/Icons/airbrush.png" },
+                new BrushItem { Name = "Oil brush", Type = BrushType.OilBrush, IconPath = "pack://application:,,,/Resources/Icons/oil_brush.png" },
+                new BrushItem { Name = "Crayon", Type = BrushType.Crayon, IconPath = "pack://application:,,,/Resources/Icons/crayon.png" },
+                new BrushItem { Name = "Marker", Type = BrushType.Marker, IconPath = "pack://application:,,,/Resources/Icons/marker.png" },
+                new BrushItem { Name = "Natural pencil", Type = BrushType.NaturalPencil, IconPath = "pack://application:,,,/Resources/Icons/natural_pencil.png" },
+                new BrushItem { Name = "Watercolor brush", Type = BrushType.WatercolorBrush, IconPath = "pack://application:,,,/Resources/Icons/watercolor.png" }
+            };
 
-            _selectedBrush = Brushes[0].Type;
-
+            if (Brushes.Count > 0)
+            {
+                SelectedBrush = Brushes[0].Type;
+            }
         }
 
         public void DoCrop() { /* logic cắt ảnh */ }
@@ -169,32 +181,5 @@ namespace PaintApplication.ViewModels
         public void DoOpenColorPicker() { /* mở popup chọn màu */ }
         public void DoAI() { /* gọi AI */ }
         public void DoLayers() { /* quản lý layers */ }
-
-        public ObservableCollection<Color> AvailableColors { get; } =
-            new ObservableCollection<Color>
-            {
-                Colors.Black,
-                Colors.White,
-                Colors.Red,
-                Colors.Green,
-                Colors.Blue,
-                Colors.Yellow,
-                Colors.Orange,
-                Colors.Purple,
-                Colors.Brown,
-                Colors.Gray,
-                Colors.Pink,
-                Colors.Cyan,
-                Colors.Magenta,
-                Colors.Lime,
-                Colors.Gold,
-                Colors.Silver,
-                Colors.Navy,
-                Colors.Teal,
-                Colors.Maroon,
-                Colors.Olive
-            };
-
-       
     }
 }
